1c27f5392e65fa1899d92f1a12413f03f0bbd5df
Dragan Belegisanin <belegisanind@gmail.com>
2025-07-18 13:08:17 +0200

    fix: add role validation in user registration process

--- services/auth_service.go
+++ services/auth_service.go
@@ -25,6 +25,11 @@ func (s *AuthService) Register(user models.User) (*models.User, error) {
 		return nil, fmt.Errorf("user not valid")
 	}
 
+	// Check if roles are allowed
+	if user.Role == models.Admin {
+		return nil, fmt.Errorf("role '%s' is not allowed for registration", user.Role)
+	}
+
 	// Check if username already exists
 	if retrieved_user, _ := s.userRepo.GetByUsername(user.Username); retrieved_user != nil {
 		return nil, fmt.Errorf("username '%s' already taken", user.Username)
